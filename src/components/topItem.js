0e3e9fbbef94772bbd7514a930d1d772b09f45e3
Takaf19 <tk.f19ms@gmail.com>
2020-06-26 00:06:28 +0900

    Update topItem.js

--- src/components/topItem.js
+++ src/components/topItem.js
@@ -11,7 +11,7 @@ export default class TopItem extends Component {
   render() {
     return (
       <div className="container">
-        <div className="container__massage">
+        <div className="container-item container__massage">
           <h1>大切な家族の<span>"万が一"</span>に備える</h1>
           <div className="container__massage--main">
             <div className="container__massage--imgbox"></div>
@@ -19,18 +19,35 @@ export default class TopItem extends Component {
               <p>私たち人間と同じように、動物にもケガや病気のリスクがあります。</p>
               <p>公的健康保険制度のないペットの診療費は"全額自己負担"となります。</p>
               <p>料金は動物病院によって決められているため、想定外に治療費が高額になるケースも少なくありません。</p>
-              <p>大切なわが子の”万が一”に備えられるのが、ペット保険の大きなメリットです。</p>
+              <p>大切なわが子の<span>”万が一”</span>に備えられるのが、ペット保険の大きなメリットです。</p>
             </div>
           </div>
         </div>
 
-        <div className="container--items">
+        <div className="container-item container--items">
           <h1>選ばれる <span>3</span>つのポイント!</h1>
-
           <div className="point">
-            <div className="point--item"></div>
-            <div className="point--item"></div>
-            <div className="point--item"></div>
+            <div className="point--item">
+              <div className="point-icon">
+
+              </div>
+              <p>１２歳まで新規加入OK!</p>
+              <p>年齢が不明な場合は、獣医師による推定年齢で加入OK</p>
+            </div>
+            <div className="point--item">
+              <div className="point-icon">
+
+              </div>
+              <p>手厚いサポート</p>
+              <p>歯科治療・先進治療・先天性の病気も保証の対象</p>
+            </div>
+            <div className="point--item">
+              <div className="point-icon">
+
+              </div>
+              <p>最短１ヶ月スタート</p>
+              <p>ご契約から最短１ヶ月で補償スタート</p>
+            </div>
           </div>
         </div>
       </div>
